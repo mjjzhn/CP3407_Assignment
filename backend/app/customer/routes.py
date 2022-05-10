eeb0fa5841a1446539b89571804fceae4486296f
Haonan Zhang <mz970611@gmail.com>
2022-05-10 14:52:22 +0800

    May 10th update
    1. Update DB with favorite list in customer table.
    2. Create Order and Order Item table in DB.
    3. Initialize the order package.
    4. Added 4 function in customer routes: registration, check out, check order, add favorite.

--- backend/app/customer/routes.py
+++ backend/app/customer/routes.py
@@ -4,7 +4,7 @@ from flask import jsonify, request
 from flask_jwt_extended import current_user
 from app.errors import bad_request
 from backend.app import db, models
-from backend.app.models import Customer
+from backend.app.models import Customer, Order, Order_item
 
 
 @bp.route('')
@@ -29,14 +29,15 @@ def update_profile():
                 return bad_request("Please provide your current password")
         if 'currentPassword' in data and not current_user.check_password(data["currentPassword"]):
             return bad_request("Current password incorrect")
-        current_user.from_dict(data)
+        current_user.set_username(data['username'])
+        current_user.set_password(data['password'])
         db.session.commit()
         response = current_user.to_dict()
         if "password" in data:
             response["is_password_updated"] = True
         return jsonify(response)
 
-@bp.route('/register', methods=["POST"])
+@bp.route('/register', methods=["POST"])#register
 @customer_required
 def customer_register():
     if request.method == 'POST':
@@ -45,10 +46,33 @@ def customer_register():
             Customer.query.filter_by(id=data['username']).first():
             return bad_request('User exists, please ues a different username')
         pwencode = data["password"].encode("utf-8")
-        '''
-        to decode the password, use password.decode("utf-8") would give you the original text
-        '''
+        #to decode the password, use password.decode("utf-8") would give you the original text
         params = dict(username=data["username"],password=pwencode)
         role = models.Customer(**params)
         db.session.add(role)
         db.session.commit()
+
+@bp.route('/check_out', methods=["POST"])#check_out
+@customer_required
+def check_out():
+    data = request.get_json() or {}
+    new_order = Order(customer_id=current_user.id)
+    new_order.set_status_order_received()
+    db.session.add(new_order)
+    db.session.commit()
+    for item in data["items"]:
+        new_order.add_item(item["id"],item["quantity"])
+    return jsonify(new_order.to_dict())
+
+@bp.route('/check_order/<int:id>', methods =["GET"])#check a order status
+def get_order(id):
+    return jsonify(Order.query.get_or_404(id).to_dict())
+
+@bp.route('/favorite>', methods=["POST"])#add one item id to customer's favorite list. Format : 1,2,5,
+def set_favorite():
+    data = request.get_json() or {}
+    current_user.set_favorite(data["item_id"])
+    response = current_user.to_dict()
+    if "favorite_list" in data:
+        response["is_favorite_list_updated"] = True
+    return jsonify(response)
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 from email.policy import default
 from zoneinfo import available_timezones
 from app import db
@@ -25,10 +26,24 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    favorite_list = db.Column(db.String(200))
 
     def __repr__(self):
         return '<Customer {}>'.format(self.username)
-    
+
+    def to_dict(self):
+        data= {
+            "id": self.id,
+            "username": self.username
+        }
+        return data
+
+    def set_favorite(self,item_id):
+        self.favorite_list = self.favorite_list + item_id + ","
+
+    def set_username(self,username):
+        self.username = username
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -145,3 +160,101 @@ class Item(APIMixin, db.Model):
         if "is_hot" in data:
             if isinstance(data["is_hot"], bool):
                 setattr(self, "is_hot", data["is_hot"])
+
+    def get_price(self):
+        price = self.item_price - self.discount
+        return price
+
+class Order(APIMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer)
+    order_status = db.Column(db.String(32))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    order_items = db.relationship('Order_item', backref='order', lazy='dynamic')
+
+    status = ['order received', 'shipping', 'delivered', 'close']
+
+    def __repr__(self):
+        return '<Order No. {}>'.format(self.id)
+
+    def add_item(self, item_id, quantity):
+        order_item = Order_item(order_id=self.id, item_id=item_id, quantity=quantity)
+        db.session.add(order_item)
+        db.session.commit()
+
+    def remove_item(self, item_id):
+        order_items = Order_item.query.filter_by(order_id=self.id, item_id=item_id).all()
+        for order_item in order_items:
+            db.session.delete(order_item)
+        db.session.commit()
+
+    def remove_all_item(self):
+        order_items = Order_item.query.filter_by(order_id=self.id).all()
+        for order_item in order_items:
+            db.session.delete(order_item)
+        db.session.commit()
+
+    def get_items(self):
+        items = []
+        for order_item in self.order_items:
+            items.append(order_item.get_item())
+        return items
+
+    def item_id_to_quantity(self):
+        item_id_to_quantity = {}
+        for order_item in self.order_items:
+            item_id_to_quantity[order_item.item_id] = order_item.quantity
+        return item_id_to_quantity
+
+    def set_status_order_received(self):
+        self.order_status = Order.status[0]
+
+    def set_status_shipping(self):
+        self.order_status = Order.status[1]
+
+    def set_status_delivered(self):
+        self.order_status = Order.status[3]
+
+    def set_status_close(self):
+        self.order_status = Order.status[4]
+
+    def to_dict(self):
+        data = {
+            "id": self.id,
+            "customer_id": self.customer_id,
+            "order_status": self.order_status,
+            "timestamp": self.timestamp
+        }
+        items = []
+        total = 0
+        for order_item in self.order_items:
+            item_to_quan = {}
+            item_to_quan["item"] = order_item.get_item().to_dict()
+            item_to_quan["quantity"] = order_item.quantity
+            items.append(item_to_quan)
+            total = total + order_item.get_item().get_price() #new defined function in item model
+
+        data["items"] = items
+        data["total"] = total
+        return data
+
+    def from_dict(self, data, add_item=False):
+        for field in ['customer_id', 'order_status']:
+            if field in data:
+                setattr(self, field, data[field])
+
+
+class Order_item(db.Model):
+    order_id = db.Column(db.Integer, db.ForeignKey("order.id"), primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey("item.id"), primary_key=True)
+    quantity = db.Column(db.Integer)
+
+    def get_item(self):
+        return Item.query.filter_by(id=self.item_id).first()
+
+    def get_order(self):
+        return Order.query.filter_by(id=self.order_id).first()
+
+    def __repr__(self):
+        return "Order_item object, order_id: {}, item_id: {}".format(self.order_id, self.item_id)
