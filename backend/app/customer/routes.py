b63db04c4723067f7573d9b13ac9fe2e9a55a97b
Haonan Zhang <mz970611@gmail.com>
2022-05-10 12:44:08 +0800

    Customer register function

--- backend/app/customer/routes.py
+++ backend/app/customer/routes.py
@@ -3,7 +3,7 @@ from app.auth.customer_routes import customer_required
 from flask import jsonify, request
 from flask_jwt_extended import current_user
 from app.errors import bad_request
-from backend.app import db
+from backend.app import db, models
 from backend.app.models import Customer
 
 
@@ -35,3 +35,20 @@ def update_profile():
         if "password" in data:
             response["is_password_updated"] = True
         return jsonify(response)
+
+@bp.route('/register', methods=["POST"])
+@customer_required
+def customer_register():
+    if request.method == 'POST':
+        data = request.get_json() or {}
+        if 'username' in data and data['username'] != current_user.username and \
+            Customer.query.filter_by(id=data['username']).first():
+            return bad_request('User exists, please ues a different username')
+        pwencode = data["password"].encode("utf-8")
+        '''
+        to decode the password, use password.decode("utf-8") would give you the original text
+        '''
+        params = dict(username=data["username"],password=pwencode)
+        role = models.Customer(**params)
+        db.session.add(role)
+        db.session.commit()
