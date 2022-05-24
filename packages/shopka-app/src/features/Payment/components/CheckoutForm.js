2162fb30b050ebbfe2b42368c0e8481c12c5f0da
peshin-ai <54382545+peshin-ai@users.noreply.github.com>
2022-05-24 22:14:42 +0800

    big update

--- packages/shopka-app/src/features/Payment/components/CheckoutForm.js
+++ packages/shopka-app/src/features/Payment/components/CheckoutForm.js
@@ -39,7 +39,7 @@ const CheckoutForm = ({ orderId }) => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: `http://localhost:3000/history`,
+        return_url: `https://shopka-client-side.herokuapp.com/history`,
       },
     });
 
